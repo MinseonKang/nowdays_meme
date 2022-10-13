27347ab889bb63a2f2138d39ae30c9361c032998
MinSeon <s_2___@naver.com>
2022-10-13 14:13:25 +0900

    project setting

---
+++ app.js
@@ -0,0 +1,24 @@
+// express 불러오기
+const express = require("express");
+
+// express 실행
+const app = express();
+
+// PORT NUMBER
+const PORT = 8000;
+
+// app에 view engine을 ejs로 설정
+app.set("view engine", "ejs");
+// ejs 파일을 저장할 views, static 폴더의 경로
+app.use("/views", express.static(__dirname + "/view"));
+app.use("/static", express.static(__dirname + "/static"));
+
+// Routing (라우팅)
+app.get("/start", function (req, res) {
+  res.render("start");
+});
+
+// 3. 로컬 서버 동작
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+});
