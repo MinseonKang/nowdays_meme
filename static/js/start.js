93027c3a9d1a2c91ea37079554de08fcbc5ca2b9
MinSeon <s_2___@naver.com>
2022-10-14 12:15:51 +0900

    start 페이지

---
+++ static/js/start.js
@@ -0,0 +1,14 @@
+const instance = new TypeIt("#pauseResume", {
+  strings:
+    "어쩔티비 어쩔 냉장고 어쩔 에어컨  어쩔 LG전자 QLED 티비 평면 스탠다드형  어쩔 1.8L 대용량 초고속 가정용 다용도 믹서기 어쩔 다이슨 프리미엄 앱솔루트 청소기 1+1 에 사은품 전자 면도기 어쩔 삼성전자 삼성 F24T350 화면크기 24인치 (59~61cm) 최대 해상도 1920x1080(FHD)최대 주사율 75Hz 패널 IPS 화면비율 16:9 응답속도 5ms 밝기 250CD 명암비 1000:1 동적명암비 무한:1 시력보호 로우블루라이트, 플리커프리 게임특화 게임모드, FreeSync 품목 게이밍모니터 색표현 NTSC(72%) 단자 D-SUB, HDMI 대기전력 0.5w 스탠드 기능 틸트(상하) vesa홀 100×100 tv어쩔 유맥스 Ai50 구글 공식인증 스마트TV크롬캐스트 종류는 LED TV화면크기 무려 127cm (50인치) 해상도 UHD(4k) 패널은 VA패널 스마트한 기능은 넷플릭스,스마트추... 2020년형 HDR10기술의 Usb기능은 USB재생 부가기능등은어쩔LG[FENIX]오브제컬렉션4도어냉장고[870L]LG 시네빔 HU85LA 4K UHD 빔프로젝터 초단초점 전자 LED 벽난로 콘솔 페치카 북유럽 크리스마스 불꽃  Jun.04 59번 마젠텍 블루투스 무선신규 초긴항운전자용 차량탑승 노이즈 남녀 귀걸이는 애플화웨이 op 적용 po 샤오미Z, 2_F600 은회색[전보이름 + 고 화질 통화 2, 정부 측 규격.삼성전자 980 PRO M.2 NVMe (1TB) SSD 어쩔LG[FENIX]오브제컬렉션4도어냉장고 M870FBB451S[870L]LG 시네빔 HU85LA 4K UHD 빔프로젝터 초단초점 전자 LED 벽난로 콘솔 페치카 북유럽 크리스마스 불꽃  Jun.04 59번 마젠텍 블루투스 무선신규 초긴항운전자용 차량탑승 노이즈 남녀 귀걸이는 애플화웨이 op 적용 po 샤오미Z, 2_F600 은회색[전보이름 + 고 화질 통화 2, 정부 측 규격.삼성전자 980 PRO M.2 NVMe (1TB) SSD삼성 비스포크 4도어 냉장고 결합형 TV어쩔 척추 의료가전 마스터 V6 블랙 MADE IN KOREA 근육통 완화, 혈액 순환 개선, 추간판탈출증 및 퇴행성협착증 의료기기 어쩔 삼성전자 비스포크 그랑데 AI 드럼세탁기 21kg + 건조기 17kg + 상단 설치 키트  WF21DV17WWL 방문설치 어쩔 삼성 비스포크 식기세척기 글램 그리너리 (싱크대 무상 리폼) / 설치배송무료 / DW60A8575LNT 어쩔 전동 전자칠판 이동식 TV거치대 55-98인치 TS-1e 어쩔 Apple 2021 애플TV 4K 64GB MXH02KH/A 어쩔 델 2020 Precision M7740 17.3 코어i7 9세대,512GB,16GB,WIN10 Pro,M7740 i7 어쩔 에이수스 2021 노트북 오리지날 블랙 라이젠7,512GB,8GB,Free DOS,ROG Strix G17 G713IE-HX009",
+  waitUntilVisible: true,
+}).go();
+
+// document.querySelector("#freezeButton").addEventListener("click", function (e) {
+//   if (instance.is("frozen")) {
+//     instance.unfreeze();
+//     return;
+//   }
+
+//   instance.freeze();
+// });
