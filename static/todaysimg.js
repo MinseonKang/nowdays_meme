f83c8211f132444185d9b67d9aa89c33005d4665
WooriSunoo <cherry21004@gmail.com>
2022-10-17 16:00:44 +0900

    study: Todya's meme's today2

--- static/todaysimg.js
+++ static/todaysimg.js
@@ -1,17 +1,44 @@
-console.log("Connected");
+console.log("Connected2");
 
-// let randomNumber = Math.floor(Math.random() * 4) + 1;
-// document.querySelector(".img_box").classList.add("bg0" + randomNumber);
-
-var imgArray = new Array();
+// 랜덤이미지 추가
+let imgArray = new Array();
 imgArray[0] = "/static/image/bg01.png";
 imgArray[1] = "/static/image/bg02.png";
 imgArray[2] = "/static/image/bg03.png";
 imgArray[3] = "/static/image/bg04.png";
+//랜덤이미지 보여주기 함수
+let timeOut; // setTimeout() 의 고유 ID 를 저장할 변수
 
 function showImage() {
-  var imgNum = Math.round(Math.random() * 3);
-  var objImg = document.getElementById("introimg");
+  let imgNum = Math.round(Math.random() * 3);
+  let objImg = document.getElementById("introimg");
   objImg.src = imgArray[imgNum];
-  setTimeout(showImage, 50);
+  // setTimeout(showImage, 100);
+  timeOut = setTimeout(showImage, 100); // setTimeout 함수 실행해서 id 1 저장
 }
+
+//이미지 클릭 시 멈춤
+
+let pauseImg = document.querySelector(".img_box");
+console.log(pauseImg);
+
+pauseImg.addEventListener("click", function (e) {
+  clearTimeout(timeOut);
+  stopImage();
+  //이미지 멈춤 함수
+  function stopImage() {
+    let imgNum2 = Math.round(Math.random() * 3);
+    let objImg2 = document.getElementById("introimg");
+    objImg2.src = imgArray[imgNum2];
+  }
+});
+
+// replay 아이콘 클릭이벤트
+
+let replayClick = document.querySelector("i");
+console.log(replayClick);
+
+replayClick.addEventListener("click", function (e) {
+  showImage();
+  replayClick.classList.toggle("bi-pause-circle"); // 멈춤 아이콘으로 변경
+});
